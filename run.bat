b309d0dc06a5e72a97566d7a9a0a42dabe0dcd9e
omerhijazi404 <omerhijazi@outlook.com>
2021-04-12 08:51:36 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -6,4 +6,4 @@ dotnet build --configuration Release
 cd ..\QBLint
 setx /M QBLINTER %CD%\QBLintLinter\bin\Release\net5.0-windows\
 dotnet run
-start "" localhost:5001
+start "" http://localhost:5001
