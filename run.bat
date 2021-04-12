e6ac513f10c066409cb926aec5a0e81be281045c
omerhijazi404 <omerhijazi@outlook.com>
2021-04-12 09:04:53 +0300

    Fixed bug in run.bat

--- run.bat
+++ run.bat
@@ -3,6 +3,7 @@ set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs") && fsutil dirty query %systemdrive% 1>nul 2>nul || ( echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && Exit /b)
 cd QBLintLinter
 dotnet build --configuration Release
-cd ..\QBLint
+cd ..\
 setx /M QBLINTER %CD%\QBLintLinter\bin\Release\net5.0-windows\
+cd QBLint
 dotnet run
