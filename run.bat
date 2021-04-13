0acdd9966c9cd906683bd7b8ee91f24ec2b51c38
omerhijazi404 <omerhijazi@outlook.com>
2021-04-13 14:29:37 +0300

    Added custom url choosing to run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs") && fsutil dirty query %systemdrive% 1>nul 2>nul || ( echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && Exit /b)
+set url="http://localhost:18390"
+set /p url="Enter a url to host QBLint at (Default: http://localhost:18390): "
 cd QBLintLinter
 dotnet restore
 dotnet build --configuration Release --no-restore
@@ -9,4 +11,4 @@ setx /M QBLINTER %CD%\QBLintLinter\bin\Release\net5.0-windows\
 cd QBLint
 dotnet restore
 dotnet build --no-restore
-dotnet run --no-build
+dotnet run --no-build --urls=%url%
