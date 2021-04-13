314d5fb1b1bdbe3d2f990e018006717e09a46d9b
omerhijazi404 <omerhijazi@outlook.com>
2021-04-13 13:23:33 +0300

    Added capitalization option

--- QBLint/Pages/Lint.cshtml.cs
+++ QBLint/Pages/Lint.cshtml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using IOFile = System.IO.File;
 
@@ -8,8 +9,23 @@ namespace QBLint.Pages
 {
     public class LintModel : PageModel
     {
-        public string Lint(string UnlintedCode) {
+        public string Lint(string UnlintedCode, string Capitalize) {
             string LintedCode = "";
+
+            ProcessStartInfo WhereInfo = new ProcessStartInfo { RedirectStandardOutput = true, Arguments = "qb64", FileName = "where.exe" };
+            Process Where = Process.Start(WhereInfo);
+            Where.WaitForExit();
+            string Config = Where.StandardOutput.ReadToEnd();
+            Config = Regex.Replace(Config, $"qb64.exe", "", RegexOptions.IgnoreCase);
+            Config = Config.Replace("\n", "");
+            Config = Config.Replace("\r", "");
+            Config += "internal\\config.ini";
+            // IDE_KeywordCapital
+            string ConfigContents = IOFile.ReadAllText(Config);
+            ConfigContents = ConfigContents.Replace("IDE_KeywordCapital=True", "IDE_KeywordCapital=" + Capitalize);
+            ConfigContents = ConfigContents.Replace("IDE_KeywordCapital=False", "IDE_KeywordCapital=" + Capitalize);
+            IOFile.WriteAllText(Config, ConfigContents);
+
             string QBLintLinterPath = Environment.GetEnvironmentVariable("QBLINTER", EnvironmentVariableTarget.Machine);
             string QBLintFolder = Path.GetTempPath() + "QBLint\\";
             if (!Directory.Exists(QBLintFolder)) {
